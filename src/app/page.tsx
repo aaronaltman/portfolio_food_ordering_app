1a588fb791cb752201a1a433b3f76dc2866bf3e9
aka <aaronkaltman@gmail.com>
2023-12-28 03:59:40 -0500

    not working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,6 @@
-import Image from "next/image";
-
-export default function Home() {
+export default function HomePage() {
   return (
     <main>
-      <header>
-        <p>logo</p>
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>Menu</li>
-            <li>Order</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-      </header>
       <h1>Next.js + TypeScript</h1>
     </main>
   );
---
+++ src/components/primary-header.tsx
@@ -0,0 +1,16 @@
+export default function PrimaryHeader() {
+  return (
+    <header>
+      <p>logo</p>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Order</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
